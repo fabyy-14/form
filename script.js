a59035d41bd7414b404978e4949961e3d74273df
fabyy-14 <fabysyeda14@gmal.com>
2026-02-06 23:28:47 +0500

    Adding All Files

---
+++ script.js
@@ -0,0 +1,5 @@
+ 
+  document.getElementById("courseForm").addEventListener("submit", function(event){
+      event.preventDefault();
+      alert("Registration Successful! Thank you for registering.");
+    });
